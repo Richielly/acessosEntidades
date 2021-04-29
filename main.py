c0253301eca3b4a9bc8520d76dc2c720c05aed6f
Richielly Carvalho <richielly.positivo@gmail.com>
2021-04-28 22:41:35 -0300

    V_1.0.1

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ import streamlit as st
 from time import sleep as s
 
 #url = 'http://187.95.124.224:7474/'
-url = st.text_input('Informe a Url')
+urls =[]
+urls = st.text_input('Informe a Url')
 sistemas = ['esadmin','stp','scf','srh','stm']
+
+arquivo = open('urls.txt','a')
+arquivo.write(urls + "\n")
+arquivo.close()
 
 if st.button("Verificar") :
 
-    for sistema in sistemas:
-
-        resposta = requests.get(url+sistema)
-        if (resposta.status_code == 200):
-            st.success("Sistema " + sistema.upper() + " no Ar.")
-        else:
-            st.error("Sistema " + sistema.upper() + " fora do Ar ou sem licença.")
-print()
+    for url in urls:
+        print(url)
+        for sistema in sistemas:
+            resposta = requests.get(str(url)+sistema)
+            if (resposta.status_code == 200):
+                st.success("Sistema " + sistema.upper() + " no Ar.")
+            else:
+                st.error("Sistema " + sistema.upper() + " fora do Ar ou sem licença.")
